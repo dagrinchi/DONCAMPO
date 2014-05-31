73ec54c95833a37ed14caeb135fad8bd4d787319
David Alméciga <wdavid@dagrinchi.com>
2014-05-31 14:54:30 -0500

    Initial Commit

---
+++ DONCAMPO/C4CAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  C4CAppDelegate.h
+//  DONCAMPO
+//
+//  Created by David Almeciga on 5/31/14.
+//  Copyright (c) 2014 David Almeciga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface C4CAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DONCAMPO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DONCAMPO
+//
+//  Created by David Almeciga on 5/31/14.
+//  Copyright (c) 2014 David Almeciga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "C4CAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([C4CAppDelegate class]));
+    }
+}
